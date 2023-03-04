ac8b010a4f324ccaaca617ed58304991b3f8937a
sopiokvantaliani <skvantaliani@gmail.com>
2023-03-04 10:58:07 -0500

    Bootstrap files in static Thymeleaf tags

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -4,6 +4,7 @@ import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller //Inside controller class we will look for end points
 @RequestMapping("/student") //class level annotation and endpoint will be concatenated with register. So, class level end-point+method level endpoint
@@ -20,6 +21,14 @@ public class StudentController {
     public String drop () {
 
         return "student/register"; //view
+    }
+
+    @RequestMapping("/welcome") //localhost:8080/student/welcome?id=3
+    public String welcome (@RequestParam String name) {
+
+        System.out.println(name);
+
+        return "student/welcome"; //view
     }
 
 }
