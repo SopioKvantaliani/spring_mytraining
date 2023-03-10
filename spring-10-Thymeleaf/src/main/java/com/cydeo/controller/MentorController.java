7950082835208e144988b472a68b0f7e86be9b33
sopiokvantaliani <skvantaliani@gmail.com>
2023-03-10 12:45:24 -0500

    retrieving data from UI

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -4,6 +4,8 @@ import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -21,6 +23,20 @@ public class MentorController {
         model.addAttribute("mentor", new Mentor());//"mentor" holds empty Object. We need to pass this object in the html 'form'
 
         return "mentor/mentor-register";
+    }
+
+    @PostMapping("/confirm") //localhost:8080/mentor/register/confirm
+    public String submitForm (@ModelAttribute("mentor") Mentor mentor) { //new syntax
+
+
+        //how can I access to "mentor" attribute in this method
+        return "mentor/mentor-confirmation";
+    }
+
+    //it is possible to have Post and GetMapping at the same time with the same endpoint
+    @GetMapping("/confirm") //localhost:8080/mentor/confirm
+    public String submitForm2 () {
+        return "mentor/mentor-confirmation";
     }
 
 
