7ee0dccfc1181acae934bf84cf1e6c75c4108d28
sopiokvantaliani <skvantaliani@gmail.com>
2023-03-10 12:04:29 -0500

    connecting to UI

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,14 +3,17 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller //Inside controller class we will look for end points
 @RequestMapping("/student") //class level annotation and endpoint will be concatenated with register. So, class level end-point+method level endpoint
-public class StudentController {
+public class StudentController { //class level we put requestMapping, we can't use GetMapping or PostMapping
 
-    @RequestMapping("/register") //localhost:8080/student/register
+
+    //   @RequestMapping(value = "/register", method = RequestMethod.GET)
+    @GetMapping("/register")  //localhost:8080/student/register
+    //  This two lines are the same
+
     public String register (Model model) {
     model.addAttribute("students", DataGenerator.createStudent());
 
@@ -23,7 +26,9 @@ public class StudentController {
         return "student/register"; //view
     }
 
-    @RequestMapping("/welcome") //localhost:8080/student/welcome?id=3
+ //   @RequestMapping("/welcome")
+    @PostMapping("/welcome") //localhost:8080/student/welcome?id=3
+    //When we post something we use PostMapping
     public String welcome () {
 
         return "student/welcome"; //view
